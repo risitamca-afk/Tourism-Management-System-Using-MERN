56021bdedbc416352963b750ed8bce2604466696
Risita Banik <risitamca@gmail.com>
2026-06-17 16:44:17 +0530

    Add files via upload

---
+++ finalproject/frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  // variants: {
+  //   extend: {
+  //     opacity: ["data-open", "data-closed"],
+  //     translate: ["data-open", "data-closed"],
+  //     borderColor: ["data-selected"],
+  //     textColor: ["data-selected"],
+  //   },
+  // },
+};
